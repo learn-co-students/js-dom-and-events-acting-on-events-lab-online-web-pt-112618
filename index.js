b6fcc5a17aea60e1f80e36a0af0dc73d66c4bb4a
yukijina <yukijina@gmail.com>
2019-06-24 06:29:32 -0700

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,27 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  return document.getElementsByTagName('input')[0].value;
+}
+
+function addNewElementAsLi() {
+  let li = document.createElement('li');
+  li.innerHTML = retrieveEmployeeInformation();
+
+  document.querySelector('.employee-list').appendChild(li)
+}
+
+function addNewLiOnClick() {
+  document.querySelector('input[type="submit"]').addEventListener('click', function() {
+    addNewElementAsLi();
+    document.querySelector('input').value = "";
+  })
+}
+
+function clearEmployeeListOnLinkClick() {
+  document.querySelector('a').addEventListener('click', function() {
+    document.querySelector('.employee-list').innerHTML = "";
+  })
+}
